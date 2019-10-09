13298d156039fc2684fc811f50b5b96b99c990ed
Avinash Kumar Prasad <itsmeavinash.prasad@gmail.com>
2019-10-09 15:18:53 +0530

    completed MovieRepositoryTest

--- src/test/java/com/stackroute/movieservices/repository/MovieRepositoryTest.java
+++ src/test/java/com/stackroute/movieservices/repository/MovieRepositoryTest.java
@@ -19,7 +19,7 @@ public class MovieRepositoryTest {
     @Autowired
     private MovieRepository movieRepository;
 
-    private Movie movie;
+    private Movie movie, movie2;
 
     @Before
     public void setUp() {
@@ -31,32 +31,63 @@ public class MovieRepositoryTest {
                 true,
                 "Overview 1"
         );
+        movie2 = new Movie(
+                2,
+                "Movie 2",
+                (float) 7.2,
+                "2009-02-02",
+                false,
+                "Overview 2"
+        );
     }
 
     @After
     public void tearDown() {
-//        movieRepository.deleteAll();
-//        this.movie = null;
+        movieRepository.deleteAll();
+        this.movie = null;
+        this.movie2 = null;
     }
 
     @Test
     public void testSaveMovie() {
         Movie fetchMovie = movieRepository.save(movie);
-        Assert.assertEquals(1, fetchMovie.getId());
+        Assert.assertEquals("Movie 1", fetchMovie.getTitle());
     }
 
     @Test
     public void testSaveMovieFailure() {
-        Movie testMovie = new Movie(
-                1,
-                "Movie 1",
-                (float) 7.1,
-                "2009-01-01",
-                true,
-                "Overview 1"
-        );
         Movie fetchMovie = movieRepository.save(movie);
-        Assert.assertNotSame(testMovie, movie);
+        Assert.assertNotSame(movie2, movie);
+    }
+
+    @Test
+    public void testDeleteByIdSuccess(){
+        Movie savedMovie = movieRepository.save(movie);
+        movieRepository.deleteById(savedMovie.getId());
+        Assert.assertFalse(movieRepository.existsById(savedMovie.getId()));
+
+    }
+
+    @Test
+    public void testDeleteByIdFailure(){
+        Movie savedMovie = movieRepository.save(movie);
+        Movie savedMovie2 = movieRepository.save(movie2);
+        movieRepository.deleteById(savedMovie.getId());
+        Assert.assertTrue(movieRepository.existsById(savedMovie2.getId()));
+    }
+
+    @Test
+    public void testGetMovieSuccess(){
+        Movie savedMovie = movieRepository.save(movie);
+        Movie fetchMovie = movieRepository.findById(savedMovie.getId()).get();
+        Assert.assertSame(fetchMovie, savedMovie);
+    }
+
+    @Test
+    public void testGetMovieFailure(){
+        Movie savedMovie = movieRepository.save(movie);
+        Movie fetchMovie = movieRepository.findById(savedMovie.getId()).get();
+        Assert.assertNotSame(fetchMovie, movie2);
     }
 
     @Test
@@ -69,14 +100,6 @@ public class MovieRepositoryTest {
                 true,
                 "Overview 1"
         );
-        Movie movie2 = new Movie(
-                2,
-                "Movie 2",
-                (float) 7.2,
-                "2009-02-02",
-                false,
-                "Overview 2"
-        );
 
         System.out.println("Saving: " + movieRepository.save(movie1).toString());
         System.out.println("Saving: " + movieRepository.save(movie2).toString());
@@ -84,6 +107,4 @@ public class MovieRepositoryTest {
         List<Movie> movieList = movieRepository.findAll();
         Assert.assertEquals("Movie 2", movieList.get(1).getTitle());
     }
-
-
 }
